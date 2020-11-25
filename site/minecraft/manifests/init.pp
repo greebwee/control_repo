f3f741e0fbcc2c5abb035969366f0308e977e7cb
greebwee <74797236+greebwee@users.noreply.github.com>
2020-11-25 12:17:02 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,9 +1,9 @@
-class minecraft {
-  $url = 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar'
+class minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar',
   $install_dir = '/opt/minecraft'
+){
   file {'/opt/minecraft':
     ensure => directory,
-  }
   file {"{$install_dir}/server.jar":
     ensure => file,
     source => $url,
